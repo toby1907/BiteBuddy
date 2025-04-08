f3aa2c5dc8540d836bf775f44f5b2449415d2d25
Tobi <olaleyetobi3@gmail.com>
2025-04-08 13:43:48 +0100

    added the necessary dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.declarative.dsl.schema.FqName.Empty.packageName
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
@@ -9,6 +10,10 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+
+    id("app.cash.sqldelight") version "2.0.2"
+
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 kotlin {
@@ -32,8 +37,27 @@ kotlin {
     
     jvm("desktop")
     
-    @OptIn(ExperimentalWasmDsl::class)
+  /*  @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
+        moduleName = "composeApp"
+        browser {
+            val rootDirPath = project.rootDir.path
+            val projectDirPath = project.projectDir.path
+            commonWebpackConfig {
+                outputFileName = "composeApp.js"
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(rootDirPath)
+                        add(projectDirPath)
+                    }
+                }
+            }
+        }
+        binaries.executable()
+    }*/
+
+    js(IR) {
         moduleName = "composeApp"
         browser {
             val rootDirPath = project.rootDir.path
@@ -58,20 +82,63 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.ktor.client.android)
+            implementation(libs.koin.android)
+            implementation("app.cash.sqldelight:android-driver:2.0.2")
         }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
+            implementation(compose.materialIconsExtended)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            //Ktor
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.logging)
+
+            //Navigation
+            implementation(libs.navigation.compose)
+
+            //Coil
+            implementation(libs.coil.compose)
+            implementation(libs.coil.network.ktor3)
+
+            //Koin
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+
+            implementation(libs.multiplatform.settings)
+implementation(libs.kotlinx.datetime)
+
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+            implementation(libs.native.driver)
+
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
             implementation(libs.kotlinx.coroutines.swing)
+
+            implementation(libs.ktor.client.java)
+            implementation(libs.sqlite.driver)
+        }
+
+        jsMain.dependencies {
+            implementation(libs.ktor.client.js)
+
+implementation(npm("@cashapp/sqldelight-sqljs-worker","2.0.2"))
+            implementation(npm("sql.js","1.8.0"))
+            implementation("app.cash.sqldelight:web-worker-driver:2.0.2")
+            implementation(devNpm("copy-webpack-plugin", "9.1.0"))
         }
     }
 }
@@ -115,6 +182,15 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "org.example.project"
             packageVersion = "1.0.0"
+        }
+    }
+}
+
+sqldelight{
+    databases{
+        create("BiteBuddyAppDb"){
+            packageName.set("org.example.project")
+            generateAsync = true
         }
     }
 }
