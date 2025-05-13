d0aa7e7640d2c7a8aabd8eb4c11bc9ade608c2a7
Tobi <olaleyetobi3@gmail.com>
2025-05-13 16:48:17 +0100

    added ktor http client

--- composeApp/src/commonMain/kotlin/org/example/project/di/NetworkModule.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/di/NetworkModule.kt
@@ -1,11 +1,12 @@
 package org.example.project.di
 
 import io.ktor.client.HttpClient
+import org.example.project.features.app.common.data.api.httpClient
 import org.koin.dsl.module
 
 fun networkModule()  = module {
 
-   /* single<HttpClient> {
+    single<HttpClient> {
         httpClient
-    }*/
+    }
 }
